e0ce82eef223b37c2f5cd563cd5ea45c8dbcd823
Abhishek-kumar09 <abhimait1909@gmail.com>
2020-12-11 01:14:34 +0530

    mini project, giphy api interaction

---
+++ index.js
@@ -0,0 +1,30 @@
+const apiKey =  "8uX3k2Q3e5ryqHgD8Jv0flcezAM1e9m6";
+
+document.querySelector("form").addEventListener('submit', (event) => {
+  event.preventDefault();
+  var searchItem = document.getElementById("search").value;
+  console.log("search item: " + searchItem);
+  const limit = 5;
+
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchItem}&limit=${limit}&offset=0`;  
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    for(var i = 0; i < data.data.length; i++) {
+
+      const gifUrl = data.data[i].images.fixed_height_downsampled.url;
+      console.log(data)
+      let fig= document.createElement("img");
+      fig.src = gifUrl;
+      
+      let out = document.querySelector("#giffy");
+      out.insertAdjacentElement('afterend', fig);
+    }
+  })
+  .catch(e => {
+    console.log("fetching");
+    console.log(e);
+  })
+
+})
