938600b29f8c3abd0b4cde356634f37d0cb771e7
Abhishek-kumar09 <abhimait1909@gmail.com>
2020-12-18 21:03:04 +0530

    remove new childs being added after the earlier ones

--- Giphy/index.js
+++ Giphy/index.js
@@ -1,30 +1,39 @@
-const apiKey =  "8uX3k2Q3e5ryqHgD8Jv0flcezAM1e9m6";
+const apiKey = "8uX3k2Q3e5ryqHgD8Jv0flcezAM1e9m6";
 
 document.querySelector("form").addEventListener('submit', (event) => {
   event.preventDefault();
+  const d = document.querySelector("#giffy");
+  removeAllChildNodes(d);
+
   var searchItem = document.getElementById("search").value;
   console.log("search item: " + searchItem);
-  const limit = 5;
+  const limit = 10;
 
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchItem}&limit=${limit}&offset=0`;  
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchItem}&limit=${limit}&offset=0`;
+
 
   fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    for(var i = 0; i < data.data.length; i++) {
+    .then(response => response.json())
+    .then(data => {
+      for (var i = 0; i < data.data.length; i++) {
 
-      const gifUrl = data.data[i].images.fixed_height_downsampled.url;
-      console.log(data)
-      let fig= document.createElement("img");
-      fig.src = gifUrl;
-      
-      let out = document.querySelector("#giffy");
-      out.insertAdjacentElement('afterend', fig);
-    }
-  })
-  .catch(e => {
-    console.log("Got some errors");
-    console.log(e);
-  })
+        const gifUrl = data.data[i].images.fixed_height_downsampled.url;
+        console.log(data)
+        let fig = document.createElement("img");
+        fig.src = gifUrl;
 
+        let out = document.querySelector("#giffy");
+        out.appendChild(fig);
+      }
+    })
+    .catch(e => {
+      console.log("Got some errors");
+      console.log(e);
+    })
 })
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+}
